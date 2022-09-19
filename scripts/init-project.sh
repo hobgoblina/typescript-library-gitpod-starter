d29c5edacc3003caf83a07b429ba3443a4668954
Lina <git@lina.pizza>
2022-09-19 03:23:25 +0000

    script comments

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -2,10 +2,12 @@
 
 yarn init
 
+# Set package entrypoints to rollup bundle outputs
 npx --yes json -I -f package.json -e "this.main=\"dist/bundle.cjs.js\""
 npx json -I -f package.json -e "this.module=\"dist/bundle.es.js\""
 npx json -I -f package.json -e "this.types=\"dist/bundle.d.ts\""
 
+# Add build, linting, testing, & lifecycle scripts
 npx --yes npm-add-script -k build -v "tsc && yarn rollup -c"
 npx npm-add-script -k lint -v "eslint src --fix"
 npx npm-add-script -k test -v "jest --config jest.config.ts"
@@ -13,8 +15,10 @@ npx npm-add-script -k preversion -v "yarn test && yarn lint"
 npx npm-add-script -k prepublishOnly -v "yarn test && yarn lint"
 npx npm-add-script -k prepare -v "husky install"
 
+# Add lint-staged config
 npx json -I -f package.json -e "this['lint-staged']={'*.ts':[\"eslint --fix\",\"git add\"]}"
 
+# Add dev dependencies
 yarn add -D \
   typescript \
   ts-node \
@@ -29,5 +33,6 @@ yarn add -D \
   husky \
   rollup \
   rollup-plugin-dts
-  
+
+# Install husky for git hooks
 npx husky install
